452c93de90f47f30a06f8971c1eb9f5fe4c7479d
baotran1109 <thaibao110904@yahoo.com>
2025-10-14 19:50:29 -0500

    }

--- job_scraper/discord.py
+++ job_scraper/discord.py
@@ -1,7 +1,10 @@
 import requests
 from job import Job
 
-WEBHOOK_URL = "YOUR_DISCORD_WEBHOOK_URL"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1427808959594500238/dUwGdZvW3enGsjZzMI4plGff7rdbfZnhvYYcufjFYXGTMld4zepZqza6_kdVc0HmkoE5"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1427809223252639805/lofOvltzPFMK8ZplWJWaEWESU9uV9QBgQASKM3ppqIr2THi5S1a6-HtYn2AlBKxjUwzu"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1427809340752007248/S7tORtU3b-e10FDlsTCQWav_UHhcNjIyV6YJxnMuUZ_oKI2MIZLUaqSVUN56a2kARF7g"
+
 
 def send_to_discord(job: Job):
     data = {
